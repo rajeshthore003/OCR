b5b1cc0509d7282d11242c83cc9a13f8b85377fb
Rajesh Thore <rajeshthore003@gmail.com>
2023-03-31 13:05:20 +0530

    corrected the dynamic path of the user input

--- minor/views.py
+++ minor/views.py
@@ -17,6 +17,8 @@ def index(request):
 
 def predicttext(request):
     if request.method == 'POST':
+        base_dir = r"C:\Users\Admin\OCR\media"
+
         file_obj = request.FILES['filepath']
         fs = FileSystemStorage()             #it will help to store the image in the System
         filePathName = fs.save(file_obj.name,file_obj)
@@ -25,13 +27,7 @@ def predicttext(request):
         print(image_name)
         imageName = image_name
         
-        # Actual OCR Code
-        # img_path = r'C:\Users\Admin\OCR\media'
-        # img = cv2.imread(r"C:\Users\Admin\OCR\media\hand2.png")
-        # cv2.imshow("IMAGE", img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # text = pytesseract.image_to_data(img)
+        file_path = os.path.join(base_dir,image_name)
 
         test_img_path = r'C:\Users\Admin\OCR\media'
         create_path = lambda f : os.path.join(test_img_path, f)
@@ -40,18 +36,22 @@ def predicttext(request):
         def show_image(img_path, size=(224, 224)):
             image = cv2.imread(img_path)
             image = cv2.resize(image, size)
+    
+            
+        base_dir = r"C:\Users\Admin\OCR\media"
 
-        image = cv2.imread(r"C:\Users\Admin\OCR\media\hand'.png") 
-        # cv2.imshow("IMAGE", image)
+        image = cv2.imread(file_path) 
+        
+        
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         avb_langs = pt.get_languages(config='hin')
-        path = create_path(r"C:\Users\Admin\OCR\media\hand'.png")
+        path = create_path(file_path)
+         
+    
         image = Image.open(path)
         
         text = pt.image_to_string(image, lang='hin')
-
-
 
         context = {'filePathName': filePathName, 'imageName':imageName,'text': text }
         return render(request,'index.html',context)
